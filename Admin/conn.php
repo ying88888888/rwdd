aecc458191c4ec7fd04cc80856bfb1d3e9d84b55
Adrianwoww <adianken383@gmail.com>
2026-04-01 16:50:49 +0800

    Add files via upload

---
+++ Admin/conn.php
@@ -0,0 +1,12 @@
+<?php
+    $localhost = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $dbName = 'events';
+
+    $dbConn = mysqli_connect($localhost, $user, $pass, $dbName);
+
+    if(mysqli_connect_errno()) {
+        die('<script>alert("Connection failed: Please check your SQL connection!");</script>');
+    }
+?>
---
+++ Admin/updateEventStatus.php
@@ -0,0 +1,37 @@
+<?php
+include "conn.php";
+
+if (isset($_GET ['id']) && isset ($_GET ['action'])){
+    $id = intval ($_GET ['id']);
+    $action = $_GET ['action'];
+
+    // echo "id: $id, action: $action";
+
+    if ($action == "approve"){
+        $status = "Approved";
+    }
+    else if ($action == "reject"){
+        $status = "Rejected";
+    }
+    else {
+        die ("Invalid action");
+    }
+
+    $sql = "UPDATE events SET status = '$status' WHERE id = $id";
+
+    if (mysqli_query($dbConn, $sql)){
+        echo "Update success: Event status changed to '$status'";
+        header ("Location: eventApproval.php");
+        exit();
+    }
+    else{
+        echo "Update failed: " . mysqli_error($dbConn);
+    }
+}
+else{
+    echo "Missing id or action";
+}
+
+
+
+?>
